b35d5693fbef7f0d85776be131f4a8aae0c7e71a
arhammusheer <arhammusheer@gmail.com>
2022-01-21 01:07:54 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
-import { ChakraProvider, Box, Flex, Spacer, theme } from "@chakra-ui/react";
+import { ChakraProvider, Box, Flex, Spacer } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Dad from "./components/Dad";
 import Name from "./components/Name";
+import theme from "./theme"
+
+import '@fontsource/raleway/700.css'
+import '@fontsource/open-sans/400.css'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
--- src/components/Dad.tsx
+++ src/components/Dad.tsx
@@ -1,8 +1,94 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Spacer,
+  Stack,
+  Heading,
+  Center,
+  IconButton,
+  Fade,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { FaSync } from "react-icons/fa";
+
+import { motion } from "framer-motion";
 
 const Dad = () => {
-	
-  return <Box>Helo</Box>
+  const [joke, setJoke] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+
+  const getJoke = async () => {
+    setIsOpen(false);
+
+    return await fetch("https://dad.croissant.one/api/joke", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setJoke(data.joke[0].content);
+        setIsOpen(true);
+      });
+  };
+
+  useEffect(() => {
+    getJoke();
+  }, []);
+
+  const AnimatedFaSync = () => (
+    <motion.div
+      whileHover={{ rotate: 20 }}
+      whileTap={{
+        rotate: -60,
+        borderRadius: "100%",
+      }}
+    >
+      <FaSync />
+    </motion.div>
+  );
+
+  return (
+    <Flex>
+      <Spacer />
+      <Box
+        border={"1px"}
+        borderRadius={"lg"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+        shadow={"md"}
+        width={{ base: "90%", md: "40rem" }}
+      >
+        <Stack direction={{ base: "column", md: "row" }}>
+          <Heading
+            size={"4xl"}
+            // textColor={"white"}
+            pt={{ base: 4, md: 20 }}
+            p={4}
+            textAlign={{ base: "left", md: "right" }}
+          >
+            Dad Bot
+          </Heading>
+          <Center w={"100%"} p={3}>
+            <Fade in={isOpen}>{joke}</Fade>
+          </Center>
+        </Stack>
+        <Flex p={1}>
+          <Spacer />
+          <IconButton
+            variant={"ghost"}
+            justifySelf={"flex-end"}
+            aria-label={"Reload"}
+            icon={<AnimatedFaSync />}
+            onClick={() => {
+              getJoke();
+            }}
+          />
+        </Flex>
+      </Box>
+      <Spacer />
+    </Flex>
+  );
 };
 
 export default Dad;
