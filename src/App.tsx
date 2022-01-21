1eb45b0f53a99d99d47689512b4da8edc13fb236
arhammusheer <arhammusheer@gmail.com>
2022-01-21 01:51:26 -0500

    about me

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { ChakraProvider, Box, Flex, Spacer } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Dad from "./components/Dad";
 import Name from "./components/Name";
-import theme from "./theme"
+import theme from "./theme";
+import AboutMe from "./components/AboutMe";
 
-import '@fontsource/raleway/700.css'
-import '@fontsource/open-sans/400.css'
+import "@fontsource/raleway/700.css";
+import "@fontsource/open-sans/400.css";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
+    <Box fontSize="xl">
       <Flex margin={3}>
         <Name />
         <Spacer />
         <ColorModeSwitcher />
       </Flex>
+      <AboutMe />
       <Box>
         <Dad />
       </Box>
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,3 +1,25 @@
-export default function AboutMe(){
-	return <></>
+import { Box, Stack, Heading, useColorModeValue, Text } from "@chakra-ui/react";
+
+export default function AboutMe() {
+  return (
+    <Box height={"90vh"} p={5}>
+      <Stack w={"100%"} h={"100%"} align={"stretch"}>
+        <Heading
+          fontSize={"11.3vw"}
+          noOfLines={1}
+          color={useColorModeValue("gray.400", "gray.600")}
+        >
+          Arham Musheer
+        </Heading>
+        <Text
+          fontSize={"6vw"}
+          noOfLines={1}
+          color={useColorModeValue("gray.400", "gray.600")}
+        >
+          A student/web developer
+        </Text>
+				<Text style={{position:"absolute", rotate:"90deg", translate:"50vw 50vh"}} fontSize={200}>🥐</Text>
+      </Stack>
+    </Box>
+  );
 }
